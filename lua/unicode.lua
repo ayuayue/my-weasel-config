003d4c2ef2fb6bf87e550023cdc4cd155280ee0b
caoayu <1401262639@qq.com>
2024-09-19 11:04:48 +0800

    rime ice

--- lua/unicode.lua
+++ lua/unicode.lua
@@ -1,32 +1,36 @@
--- Unicode quick input
-function Unicode_Translator(input, seg, env)
-	if not (input:match("^U") or input:match("^/uc")) then return end
-	local segment = env.engine.context.composition:back()
-	if input:match("^U$") or input:match("^/uc$") then
-		segment.prompt = "输入[0-9a-fA-F],以[^0-9a-fA-F]分割两个字符，Unicode"
-	end
-	if input:match("^U%x+") or input:match('^/uc%x+') then
-		local cand = ''
-		local cmt = ''
-		local rinput = ''
-		if string.match(input, "^/uc%x+") then
-			rinput = string.sub(input, 4)
-		else
-			rinput = string.sub(input, 2)
-		end
-		for s in string.gmatch(rinput, '%x+') do
-			if(tonumber(s, 16) >= 0x20 and tonumber(s, 16) <= 0x10ffff) then
-				cand = cand .. utf8.char(tonumber(s, 16))
-				cmt = cmt .. '\\u+' .. string.upper(s)
-			end
-		end
-		if cand ~= "" then
-			local cc = Candidate("Unicode", seg.start, seg._end, cand, cmt)
-			yield(cc)
-		end
-	end
+-- Unicode
+-- 复制自： https://github.com/shewer/librime-lua-script/blob/main/lua/component/unicode.lua
+-- 示例：输入 U62fc 得到「拼」
+-- 触发前缀默认为 recognizer/patterns/unicode 的第 2 个字符，即 U
+-- 2024.02.26: 限定编码最大值
+-- 2024.06.01: 部分变量初始化，条件语句调整。
+
+local path = 'recognizer/patterns/unicode'
+local function unicode(input, seg, env)
+    if not seg:has_tag("unicode") or input == '' then return end
+    -- 获取 recognizer/patterns/unicode 的第 2 个字符作为触发前缀
+    -- config:get_string(path) 可能取得 nil 造成error
+    if not env.unicode_keyword then
+        local pattern = env.engine.schema.config:get_string(path) or "UU"
+        env.unicode_keyword = pattern:sub(2,2)
+    end
+
+    local ucodestr = input:match(env.unicode_keyword .. "(%x+)")
+    if ucodestr and #ucodestr > 1 then
+        local code = tonumber(ucodestr, 16)
+        if code > 0x10FFFF then
+           yield(Candidate("unicode", seg.start, seg._end, "数值超限！", ""))
+           return
+        end
+        local text = utf8.char(code)
+        yield(Candidate("unicode", seg.start, seg._end, text, string.format("U%x", code)))
+        if code < 0x10000 then
+           for i = 0, 15 do
+               local text = utf8.char(code * 16 + i)
+                yield(Candidate("unicode", seg.start, seg._end, text, string.format("U%x~%x", code, i)))
+           end
+        end
+    end
 end
 
-function Unicode_Tips()
-	return "U\\x+\\X+\\x+ ...\tUnicode\r/uc\\x+\\X+\\x+ ...\tUnicode"
-end
+return unicode
