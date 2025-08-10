11ec33a3490042ca973d7f3f67c0047e39dbb94f
caoayu <1401262639@qq.com>
2025-08-11 01:08:21 +0800

    init

--- lua/input_stats.lua
+++ lua/input_stats.lua
@@ -1,40 +1,42 @@
 input_stats = {
-    ["weekly"] = {
-    ["length"] = 138,
-    ["count"] = 75,
-    ["fastest"] = 33,
+    ["lengths"] = {
+    [3] = 2,
+    [4] = 1,
+    [2] = 2,
+},
+    ["yearly"] = {
+    ["ts"] = 1735660800,
+    ["count"] = 5,
+    ["fastest"] = 7,
+    ["length"] = 14,
+},
+    ["daily_max"] = 4,
+    ["daily"] = {
     ["ts"] = 1754841600,
+    ["count"] = 5,
+    ["fastest"] = 7,
+    ["length"] = 14,
+},
+    ["monthly"] = {
+    ["ts"] = 1753977600,
+    ["count"] = 5,
+    ["fastest"] = 7,
+    ["length"] = 14,
+},
+    ["weekly"] = {
+    ["ts"] = 1754841600,
+    ["count"] = 5,
+    ["fastest"] = 7,
+    ["length"] = 14,
 },
     ["recent"] = {
     [1] = {
+    ["len"] = 4,
+    ["ts"] = 1754845681,
+},
+    [2] = {
     ["len"] = 3,
-    ["ts"] = 1754844768,
+    ["ts"] = 1754845690,
 },
-},
-    ["yearly"] = {
-    ["length"] = 138,
-    ["count"] = 75,
-    ["fastest"] = 33,
-    ["ts"] = 1735660800,
-},
-    ["daily_max"] = 6,
-    ["daily"] = {
-    ["length"] = 138,
-    ["count"] = 75,
-    ["fastest"] = 33,
-    ["ts"] = 1754841600,
-},
-    ["monthly"] = {
-    ["length"] = 138,
-    ["count"] = 75,
-    ["fastest"] = 33,
-    ["ts"] = 1753977600,
-},
-    ["lengths"] = {
-    [1] = 32,
-    [2] = 29,
-    [3] = 10,
-    [4] = 3,
-    [6] = 1,
 },
 }
